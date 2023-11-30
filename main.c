035457851cae3454394accd37e641c8313a76450
양지원 <101974118+aqua6191@users.noreply.github.com>
2023-11-30 15:28:41 +0900

    02

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-
-struct student {
-    int student_id;
-    char name[50];
-    float grade;
+struct point {
+    int      x;
+    int      y;
 };
 
 int main(void) {
     // 구조체 변수 생성
-    struct student s;
-
-    // 학생 정보 입력
-    s.student_id = 2114034;
-    strcpy(s.name, "양지원");
-    s.grade = 3.5;
-
-    // 학생 정보 출력
-    printf("ID: %d\n", s.student_id);
-    printf("name: %s\n", s.name);
-    printf("grade: %.2f\n", s.grade);
-
+    struct point p1,p2;
+    int xdiff, ydiff;
+    double dist;
+    
+    printf("input p1 coordinate (x,y) :");
+    scanf("%d %d", &p1.x, &p1.y);
+    printf("input p2 coordinate (x,y) :");
+    scanf("%d %d", &p2.x, &p2.y);
+    
+    
+    xdiff = p2.x - p1.x;
+    ydiff = p2.y - p1.y;
+    dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+    
+    printf("distance : %f\n", dist);
+   
+   
     system("PAUSE");
     return 0;
 }
